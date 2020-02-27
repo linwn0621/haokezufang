9f022d53b26773c0398d51c3b9e78b4de59b419f
jackyfive <1833792912@qq.com>
2020-02-27 21:53:27 +0800

    首页轮播图完成

--- src/pages/home/index/index.js
+++ src/pages/home/index/index.js
@@ -1,14 +1,54 @@
-import React from "react"
+import { Carousel } from "antd-mobile";
+import React from "react";
+import axios from "axios";
 class Index extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>首页</div>
-         );
-    }
+  state = {
+    data: ["1", "2", "3"],
+    swiperList: []
+    // imgHeight: 176,
+  };
+  componentDidMount() {
+    this.getSwiperList();
+  }
+
+  // 获取轮播图的数据
+  getSwiperList = () => {
+    axios.get("http://157.122.54.189:9060/home/swiper").then(res => {
+      console.log(res);
+      this.setState({
+        swiperList: res.data.body
+      });
+    });
+  };
+
+  render() {
+    return (
+      this.state.swiperList.length&&<Carousel autoplay infinite>
+      
+        {this.state.swiperList.map(val => (
+          <a
+            key={val.id}
+            href="http://www.alipay.com"
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: this.state.imgHeight
+            }}
+          >
+            <img
+              src={"http://157.122.54.189:9060" + val.imgSrc}
+              alt=""
+              style={{ width: "100%", verticalAlign: "top" }}
+              onLoad={() => {
+                // fire window resize event to change height
+                window.dispatchEvent(new Event("resize"));
+                this.setState({ imgHeight: "auto" });
+              }}
+            />
+          </a>
+        ))}
+      </Carousel>
+    );
+  }
 }
- 
 export default Index;
