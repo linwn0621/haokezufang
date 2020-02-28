bee2108756a2c280ee0be53a1848c80587f69a18
jackyfive <1833792912@qq.com>
2020-02-28 11:25:53 +0800

    首页租房小组部分完成

--- src/pages/home/index/index.js
+++ src/pages/home/index/index.js
@@ -8,6 +8,7 @@ import nav4 from "../../../assets/images/nav-4.png";
 import IndexCss from "./index.module.scss";
 class Index extends React.Component {
   state = {
+    swiperList: [],
     navs: [
       { id: 1, imgSrc: nav1, text: "整租" },
       {
@@ -22,11 +23,13 @@ class Index extends React.Component {
       },
       { id: 4, imgSrc: nav4, text: "去出租" }
     ],
-    swiperList: []
+    // 租房 数据
+    rentings: []
     // imgHeight: 176,
   };
   componentDidMount() {
     this.getSwiperList();
+    this.getZufanglist();
   }
 
   // 获取轮播图的数据
@@ -38,7 +41,18 @@ class Index extends React.Component {
       });
     });
   };
-
+  // 获取租房小组数据
+  getZufanglist = () => {
+    axios
+      .get(
+        "http://157.122.54.189:9060/home/groups?area=AREA%7C88cff55c-aaa4-e2e0"
+      )
+      .then(res => {
+        this.setState({
+          rentings: res.data.body
+        });
+      });
+  };
   render() {
     return (
       <div className={IndexCss.Index}>
@@ -72,7 +86,7 @@ class Index extends React.Component {
           )}
         </div>
         {/* 轮播图结束 */}
-      
+
         {/* 首页导航 开始 */}
         <div className={IndexCss.IndexNav}>
           {this.state.navs.map(v => (
@@ -83,6 +97,30 @@ class Index extends React.Component {
           ))}
         </div>
         {/* 首页导航 结束 */}
+
+        {/* 首页租房开始 */}
+        <div className={IndexCss.IndexZf}>
+          {/* 头部 */}
+          <div className={IndexCss.Zftitle}>
+            <span>租房小组</span>
+            <a href="#">更多</a>
+          </div>
+          {/* 内容 */}
+          <div className={IndexCss.Zfcontent}>
+            {this.state.rentings.map(v => (
+              <div key={v.id} className={IndexCss.Zfitem}>
+                <div className={IndexCss.item_text}>
+                  <div className={IndexCss.text_title}>{v.title}</div>
+                  <div className={IndexCss.text_desc}>{v.desc}</div>
+                </div>
+                <div className={IndexCss.item_img}>
+                  <img src={"http://157.122.54.189:9060"+v.imgSrc} alt="" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* 首页租房结束 */}
       </div>
     );
   }
