2d4250daef93d9eded715c459e5a45ee80aabb1d
jackyfive <1833792912@qq.com>
2020-03-02 22:26:20 +0800

    给搜索框城市添加点击跳转事件

--- src/components/cityinput/index.js
+++ src/components/cityinput/index.js
@@ -2,16 +2,19 @@ import React,{Component} from "react"
 import indexCss from "./index.module.scss"
 // 引入仓库链接器
 import {connect} from"react-redux"
+// 引入路由增强
+import {withRouter} from "react-router-dom"
 class Cityinput extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
+        console.log(props,3)
     }
     render() { 
         return ( 
           <div className={indexCss.Cityinput}>
               <div className={indexCss.inputLeft}>
-                  <div className={indexCss.inputLeft_city}>
+                  <div className={indexCss.inputLeft_city} onClick={()=>{this.props.history.push("/cityChooese")}}>
                       <span>{this.props.cityName}</span>
                   <i className={" iconfont icon-arrow " + indexCss["icon-arrow"]}></i>
                   </div>
@@ -34,4 +37,4 @@ const mapStoreToProps=(state)=>{
     }
         }
  
-export default connect(mapStoreToProps,null)(Cityinput);
+export default withRouter(connect(mapStoreToProps,null)(Cityinput));
---
+++ src/pages/citychooese/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+class cityChooese extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <>
+            <div>城市选择</div>
+            </>
+         );
+    }
+}
+ 
+export default cityChooese;
